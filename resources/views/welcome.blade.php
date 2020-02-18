d4f6ceed2a3aba265a0c14d02594cb01199137f4
kumaya <kumaya1109@gmail.com>
2020-02-19 00:06:34 +0900

    追加:　layout template 追加、BootStrapを使ってみる

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>Larabel 練習</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <!-- Styles -->
-        <style>
-        </style>
-    </head>
-    <body>
-         <h1 align="center"">Larabel 練習</h1>
-    </body>
-</html>
+@section('content')
+    <div class="jumbotron">
+        <h1> Laravelはすごい</h1>
+        <p>明日は餃子を食べよう。本当に。</p>
+        <p>フォント変更した方がいいかも</p>
+    </div>
+@endsection
